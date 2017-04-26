f17852fb50a138ecf6ed7bd414335e83027795ab
amarinas <ali.marinas@icloud.com>
2017-04-25 20:51:20 -0700

    created new pics action and view filles

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,25 @@
 class PicsController < ApplicationController
   def index
   end
-  
+  def new
+    @pic = Pic.new
+  end
+  def create
+    @pic = Pic.new(pic_params)
+  end
+
+  def edit
+    #code
+  end
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def pic_params
+      params.require(:pic).permit(:title, :description)
+    end
+
 end
